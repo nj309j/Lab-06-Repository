9f323a0b9cfda4bba139d7262ee00fdea266eab2
unknown <116223350+nj309j@users.noreply.github.com>
2022-10-25 12:53:36 -0400

    add subtract method

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-def add (a, b):
+def add(a, b):
     return a + b
+
+
+def subtract(a, b):
+    return a - b
+
 
 if __name__ == "__main__":
     num1 = int(input("Please input your first number: "))
     num2 = int(input("Please input your second number: "))
     sum = add(num1, num2)
+    difference = subtract(num1, num2)
     print(f"{num1} + {num2} = {sum}")
+    print(f"{num1} + {num2} = {difference}")
