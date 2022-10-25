2cc874e3ba67120f583d4a65e2c556a29386eefa
unknown <116223350+nj309j@users.noreply.github.com>
2022-10-25 12:42:13 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def add (a, b):
+    return a + b
+
+if __name__ == "__main__":
+    num1 = int(input("Please input your first number: "))
+    num2 = int(input("Please input your second number: "))
+    sum = add(num1, num2)
+    print(f"{num1} + {num2} = {sum}")
